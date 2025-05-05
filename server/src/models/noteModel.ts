e80726b3e7c49ce9a7ae733428536c8adad1aa8d
Ollie <olliemoss321@gmail.com>
2025-05-05 18:15:13 +1200

    Fixed note creation

--- server/src/models/noteModel.ts
+++ server/src/models/noteModel.ts
@@ -14,8 +14,9 @@ export type Note = {
 }
 
 export const NoteSchema = new Schema<INote>({
-    title: { type: String, required: true },
-    contents: { type: String, required: true },
+    title: { type: String, required: false },
+    // Setter ensures that if the contents is nothing it is set to a json object
+    contents: { type: String, required: false, set: (c: any) => !c || c == "" ? "{}" : c },
     uid: { type: Schema.Types.ObjectId, required: true },
 });
 
