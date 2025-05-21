6a93a07374db5b3e84441fea485153d98ef8bbaf
Ollie <olliemoss321@gmail.com>
2025-05-21 20:27:35 +1200

    Search now loads note content from backend

--- client/src/pages/notes/notesPage.tsx
+++ client/src/pages/notes/notesPage.tsx
@@ -1,14 +1,10 @@
 import Sidebar from "../../components/sidebar";
-import { useEffect, useMemo, useRef, useState } from "react";
 import Editor from "../../components/editor";
 import { useQueryParams } from "../../lib/useQueryParams";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NotesHomeSection from "../../components/notesHomeSection";
 import { SearchProvider } from "../../lib/searchProvider";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchNoteAsync, noteMapSelector } from "../../reducers/noteReducer";
-import { Note } from "../../models/note";
-import { AppDispatch } from "../../store";
+import useNoteAsync from "../../hooks/useNoteAsync";
 
 const queryClient = new QueryClient()
 
@@ -36,23 +32,7 @@ export default function Notes({ home }: { home: boolean }) {
 function NoteDisplay({ noteId }: { noteId: string }) {
     // this ensures that the note editor will only rerender when navigating to a
     // different note and note when updates occur to the note
-    const notes = useSelector(noteMapSelector)
-    const [note, setNote] = useState<Note | null>(null)
-    const noteIdRef = useRef(null);
-
-    const dispatch: AppDispatch = useDispatch()
-
-    useEffect(() => {
-        if (noteIdRef.current == noteId) return
-        if (!notes[noteId]) return
-        if (!notes[noteId].hasOwnProperty("contents")) {
-            dispatch(fetchNoteAsync(noteId))
-            return
-        }
-        noteIdRef.current = noteId
-        setNote(notes[noteId])
-
-    }, [noteId, notes])
+    const { note } = useNoteAsync(noteId)
 
     return (
         <div className="h-full w-full bg-bg">
@@ -64,4 +44,3 @@ function NoteDisplay({ noteId }: { noteId: string }) {
         </div >
     )
 }
-
