9cc07bf537f4c067e9dc6b9b3956371a2551f859
Ollie Moss <olliemoss321@gmail.com>
2025-05-28 17:22:57 +1200

    Added signup and login pages

--- client/src/pages/auth/loginPage.tsx
+++ client/src/pages/auth/loginPage.tsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
 import Header from "../../components/header";
 
 // Placeholder login page
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const login = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        //signInWithEmailAndPass(email, password)
+
+    }
     return (
         <div className="min-h-full bg-bg-dark text-white flex flex-col">
             <Header />
-            <h1>Login</h1>
+            <div className="flex justify-center">
+                <form onSubmit={login} className="py-6 px-10 rounded-lg flex flex-col bg-background-variant w-[90%] lg:w-[40%]">
+                    <h1 className="text-lg font-bold">Login</h1>
+
+                    <div className="mt-10 mb-5 flex flex-col">
+                        <label className="pb-1 font-light text-white">Email:</label>
+                        <input
+                            className="mb-3 border-background border-[1px] box-border border-transparent outline-none focus:border-white w-full bg-bg-darker text-white p-2 rounded-md text-sm"
+                            autoComplete="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label className="pb-1 font-light text-white">Password:<br /></label>
+                        <input
+                            className="mb-3 border-background border-[1px] box-border border-transparent outline-none focus:border-white w-full bg-bg-darker text-white p-2 rounded-md text-sm"
+                            autoComplete="current-password"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <section className="w-fit">
+                            <input
+                                id="remember-me"
+                                className="my-auto transition-[border] duration-75 ease-in-out checked:border-4 checked:border-green-400 bg-transparent text-accent appearance-none border-2 border-white rounded-full w-[0.75rem] h-[0.75rem]"
+                                type="checkbox"
+                            />
+                            <label htmlFor="remember-me" className="mb-3 select-none font-light text-white inline my-auto">
+                                &nbsp;Remember Me
+                            </label>
+                        </section>
+                    </div>
+                    <button
+                        className="w-full p-1 py-2 rounded-md bg-white text-bg-dark"
+                    >Login</button>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/pages/auth/signupPage.tsx
+++ client/src/pages/auth/signupPage.tsx
@@ -1,11 +1,69 @@
+import { useState } from "react";
 import Header from "../../components/header";
 
 // Placeholder signup page
 export default function Signup() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [reEnterPassword, setReEnterPassword] = useState<string>("")
+    const [error, setError] = useState("")
+
+    const signup = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (password === reEnterPassword) { }
+    }
+
     return (
         <div className="min-h-full bg-bg-dark text-white flex flex-col">
             <Header />
-            <h1>Signup</h1>
+            <div className="flex justify-center">
+                <form onSubmit={signup} className="py-6 px-10 rounded-lg flex flex-col bg-background-variant w-[90%] lg:w-[40%]">
+                    {error !== "" ? <p className="text-red-500">{error}</p> : <></>}
+                    <h1 className="text-lg font-bold">Signup</h1>
+
+                    <div className="mt-10 mb-5 flex flex-col">
+                        <label className="pb-1 font-light text-white">Email:</label>
+                        <input
+                            className="mb-3 border-background border-[1px] box-border border-transparent outline-none focus:border-white w-full bg-bg-darker text-white p-2 rounded-md text-sm"
+                            autoComplete="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label className="pb-1 font-light text-white">Password:<br /></label>
+                        <input
+                            className="mb-3 border-background border-[1px] box-border border-transparent outline-none focus:border-white w-full bg-bg-darker text-white p-2 rounded-md text-sm"
+                            autoComplete="current-password"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label className="pb-1 font-light text-white">Re-Enter Password:<br /></label>
+                        <input
+                            className="mb-3 border-background border-[1px] box-border border-transparent outline-none focus:border-white w-full bg-bg-darker text-white p-2 rounded-md text-sm"
+                            placeholder="Re-enter Password"
+                            type="password"
+                            value={reEnterPassword}
+                            onChange={(e) => {
+                                setReEnterPassword(e.target.value)
+                                if (e.target.value !== password && e.target.value !== "") {
+                                    setError("Passwords do not match!")
+                                }
+                                else if (e.target.value === password && error == "Passwords do not match!") {
+                                    setError("")
+                                }
+                            }}
+                        />
+                    </div>
+
+
+
+                    <button
+                        className="w-full p-1 py-2 rounded-md bg-white text-bg-dark"
+                    >Login</button>
+                </form>
+            </div>
         </div>
     )
 }
